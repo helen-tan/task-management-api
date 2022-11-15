b5ec68cee1441428b2486dfa82da6c0003c84d56
helen-tan <helentanmz0909@gmail.com>
2022-11-15 13:50:42 +0800

    Created task routes & controller folder

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const dotenv = require('dotenv'.config())
 const PORT = process.env.PORT || 5000
 
+// Route imports
+const applicationRoutes = require('./routes/applicationRoutes')
+const taskRoutes = require('./routes/taskRoutes')
+
 const app = express()
+
+// Routes
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'Welcome to the Task Management API' })
+})
+
+app.use('/api/tasks', taskRoutes)
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`))
