7c9d3831ac597eb93d73616600ec0b2c9d5a5a69
helen-tan <helentanmz0909@gmail.com>
2022-11-16 10:28:04 +0800

    getTasksByState login and query logic

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -73,13 +73,13 @@ const createTask = catchAsyncErrors(async (req, res) => {
         let tasksArr = await getAppTaskIds(applicationName) //  [ { task_id: 'NewTestApp_57' }, { task_id: 'NewTestApp_58' } ]
         //let tasksArr = [ { task_id: 'KlookMVP_1' }, { task_id: 'KlookMVP_2' } ]
         //console.log(tasksArr)
-    
+
         let taskIdRnumArr = []
         tasksArr.forEach((task) => {
             taskIdRnumArr.push(parseInt(task.task_id.split("_")[1]))
         })
         //console.log(taskIdRnumArr)
-    
+
         // Get r_number of app with helper method
         let r_num = await getRnumber(applicationName)
         let new_r_num
@@ -89,34 +89,34 @@ const createTask = catchAsyncErrors(async (req, res) => {
         } else {
             new_r_num = Math.max(...taskIdRnumArr) + 1 // Increment the Rnumber based on the largest exisitng one
         }
-    
+
         // Construct task_id: <app_acronym>_<app_rnumber>
         let task_id = `${applicationName}_${new_r_num}`;
-    
+
         let today = new Date();
         let dd = String(today.getDate()).padStart(2, '0');
         let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
         let yyyy = today.getFullYear();
-    
+
         let hours = today.getHours()
         let mins = today.getMinutes()
         let seconds = today.getSeconds()
-    
+
         today = yyyy + '-' + mm + '-' + dd;
-    
+
         // Validation: Regex to validate user input
         const task_nameRegexp = /^(?! )[A-Za-z0-9._\s]{2,20}(?<! )$/          // only alphanumeric, dots, underscores, spaces in between, no leading & trailing spaces, min 2 mx 20 chars
-    
+
         if (!taskName.match(task_nameRegexp)) {
             return res.status(200).send({
                 success: false,
                 message: 'Please give a valid task name'
             })
         }
-    
+
         // Construct task notes string
         let task_notes = `${username} has created the task: ${taskName} [${today} ${hours}:${mins}:${seconds}]`
-    
+
         let task_name = taskName
         let task_description = taskDescription
         let task_plan = "" // At task creation, there will be no plan
@@ -125,10 +125,10 @@ const createTask = catchAsyncErrors(async (req, res) => {
         let task_creator = username
         let task_owner = username
         let task_createdate = today
-    
+
         // Increase the current app_rnumber to prep for incrementing the app_rnumber in the db
         let new_app_rnumber = r_num + 1
-    
+
         let new_task = {
             task_id,
             task_name,
@@ -143,7 +143,7 @@ const createTask = catchAsyncErrors(async (req, res) => {
         }
         //console.log(new_task)
         //res.send(new_task)
-    
+
         // Create new task
         db.query(`insert into tasks (
             task_id,
@@ -177,7 +177,7 @@ const createTask = catchAsyncErrors(async (req, res) => {
                     code: "CT00",
                     task_id: task_id,
                 })
-    
+
                 // Increment application r_number
                 db.query(`UPDATE applications 
                 SET app_rnumber = ?
@@ -190,7 +190,7 @@ const createTask = catchAsyncErrors(async (req, res) => {
                         console.log(`r_number of ${task_app_acronym} increased from ${r_num} to ${new_app_rnumber}`)
                     }
                 })
-    
+
                 console.log(new_task)
             }
         })
@@ -202,7 +202,71 @@ const createTask = catchAsyncErrors(async (req, res) => {
 // @route   /api//tasks/getTasksByState
 // @access  Private
 const getTasksByState = catchAsyncErrors(async (req, res) => {
-    res.send("Hello")
+    const {
+        username,
+        password,
+        applicationName,
+        taskState
+    } = req.body
+
+    // query database for the user with these login credentials
+    db.query('select * from users where username = ? ', [username], async (err, results) => {
+        //console.log(results)
+        if (err) {
+            return res.send({
+                code: "GT01"
+            })
+        } else {
+            if (results.length > 0) {
+                // Validation - Check that the is_active property is true first, otherwise prevent login
+                if (!results[0].is_active) {
+                    console.log("1")
+                    return res.send({
+                        code: "GT01"
+                    })
+                }
+                // If user is active, begin authenticating
+                const comparison = await bcrypt.compare(password, results[0].password)
+
+                if (comparison) {
+                    console.log("2")
+                    console.log({
+                        message: 'Login successful',
+                        data: results,
+                    });
+
+                    getTasksByStateAuthenticated()
+                } else {
+                    // username & password do not match
+                    console.log("3")
+                    return res.send({
+                        code: "GT01"
+                    })
+                }
+            } else {
+                // username does not exist
+                console.log("4")
+                res.send({
+                    code: "GT01"
+                })
+            }
+        }
+    })
+
+    const getTasksByStateAuthenticated = async () => {
+        db.query(`select * from tasks where task_app_acronym = ? and task_state = ?`, [applicationName, taskState], (err, results) => {
+            if (err) {
+                res.send({
+                    code: "GT02"
+                })
+            } else {
+                res.send({
+                    code: "GT00",
+                    tasks: results
+                })
+            }
+        })
+    }
 })
 
 module.exports = {
