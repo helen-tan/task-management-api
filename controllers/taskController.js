693b9f00fd5926ea8b61183f2ad80960b0ab85e8
helen-tan <helentanmz0909@gmail.com>
2022-11-16 15:49:46 +0800

    Include catch all error for create task api

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -10,17 +10,18 @@ const { getAppTaskState } = require('../utils/getAppTaskState')
 const { getAppPermitCreate } = require('../utils/getAppPermitCreate')
 const { getAppPermitDoing } = require('../utils/getAppPermitDoing')
 const { getUserGroups } = require('../utils/getUserGroups')
-const { checkGroup } = require('../utils/checkGroup')
 const { getAppAcronym } = require('../utils/getAppAcronym')
+// const { checkGroup } = require('../utils/checkGroup')
 
 // @desc    Create a task (Note: An app must already exist in the DB. Please check in DB)
 // @route   /api//tasks/createTask
 // @access  Private
-const createTask = catchAsyncErrors(async (req, res) => {
+const createTask = async (req, res) => {
     // task_plan Can't be assigned yet. Only user of group in app_permit_create can. Default to be ""
     // task_notes - to be seen in view, and added to when task is first created
     // task_state - open by default
     // task_owner - by deafult the user who created
+
     const {
         username,
         password,
@@ -29,201 +30,220 @@ const createTask = catchAsyncErrors(async (req, res) => {
         taskDescription
     } = req.body
 
-    // query database for the user with these login credentials
-    db.query('select * from users where username = ? ', [username], async (err, results) => {
-        //console.log(results)
-        if (err) {
-            return res.send({
-                code: "CT01"
-            })
-        } else {
-            if (results.length > 0) {
-                // Validation - Check that the is_active property is true first, otherwise prevent login
-                if (!results[0].is_active) {
-                    console.log("1")
-                    return res.send({
-                        code: "CT01"
-                    })
-                }
-                // If user is active, begin authenticating
-                const comparison = await bcrypt.compare(password, results[0].password)
-
-                if (comparison) {
-                    console.log("2")
-                    console.log({
-                        message: 'Login successful',
-                        data: results,
-                    });
-
-                    createTaskAuthenticated()
-                } else {
-                    // username & password do not match
-                    console.log("3")
-                    return res.send({
-                        code: "CT01"
-                    })
-                }
+    try {
+        // req = abc // Use this to induce the catch all error
+        // query database for the user with these login credentials
+        db.query('select * from users where username = ? ', [username], async (err, results) => {
+            //console.log(results)
+            if (err) {
+                console.log(err)
+                return res.send({
+                    code: "CT01"
+                })
             } else {
-                // username does not exist
-                console.log("4")
-                res.send({
+                if (results.length > 0) {
+                    // Validation - Check that the is_active property is true first, otherwise prevent login
+                    if (!results[0].is_active) {
+                        console.log("1")
+                        return res.send({
+                            code: "CT01"
+                        })
+                    }
+                    // If user is active, begin authenticating
+                    const comparison = await bcrypt.compare(password, results[0].password)
+
+                    if (comparison) {
+                        console.log("2")
+                        console.log({
+                            message: 'Login successful',
+                            data: results,
+                        });
+
+                        createTaskAuthenticated()
+                    } else {
+                        // username & password do not match
+                        console.log("3")
+                        return res.send({
+                            code: "CT01"
+                        })
+                    }
+                } else {
+                    // username does not exist
+                    console.log("4")
+                    res.send({
+                        code: "CT01"
+                    })
+                }
+            }
+        })
+
+    } catch (err) {
+        console.log('Activated catch all in createTask login')
+        console.log(err)
+        res.send({
+            code: "CT99"
+        })
+    }
+
+
+    const createTaskAuthenticated = async () => {
+        try {
+            // Check and see if user's groups is in the app's app_permit_create. Only users in the group specified by app_permit_create can create tasks
+            // Get the app's app_permit_create
+            const app_permit_create = await getAppPermitCreate(applicationName)
+            // console.log(app_permit_create)
+
+            // Check the user's groups (an array)
+            const user_groups = await getUserGroups(username)
+            // console.log(user_groups)
+
+            // Check if the group in app_permit_create is in the user's groups
+            let permitted = false
+            if (user_groups.includes(app_permit_create)) {
+                permitted = true
+                console.log(`The user ${username} is in the app_permit_create of the app ${applicationName} and hence is allowed to create tasks`)
+            } else {
+                console.log(`The user ${username} is not in the app_permit_create of the app ${applicationName} and hence cannot create tasks`)
+                return res.send({
                     code: "CT01"
                 })
             }
-        }
-    })
+            // console.log(permitted)
 
-    const createTaskAuthenticated = async () => {
-        // Check and see if user's groups is in the app's app_permit_create. Only users in the group specified by app_permit_create can create tasks
-        // Get the app's app_permit_create
-        const app_permit_create = await getAppPermitCreate(applicationName)
-        // console.log(app_permit_create)
+            // Get existing task_ids of tasks in the app
+            let tasksArr = await getAppTaskIds(applicationName) //  [ { task_id: 'NewTestApp_57' }, { task_id: 'NewTestApp_58' } ]
+            //let tasksArr = [ { task_id: 'KlookMVP_1' }, { task_id: 'KlookMVP_2' } ]
+            //console.log(tasksArr)
 
-        // Check the user's groups (an array)
-        const user_groups = await getUserGroups(username)
-        // console.log(user_groups)
+            let taskIdRnumArr = []
+            tasksArr.forEach((task) => {
+                taskIdRnumArr.push(parseInt(task.task_id.split("_")[1]))
+            })
+            //console.log(taskIdRnumArr)
 
-        // Check if the group in app_permit_create is in the user's groups
-        let permitted = false
-        if (user_groups.includes(app_permit_create)) {
-            permitted = true
-            console.log(`The user ${username} is in the app_permit_create of the app ${applicationName} and hence is allowed to create tasks`)
+            // Get r_number of app with helper method
+            let r_num = await getRnumber(applicationName)
+            let new_r_num
+            if (taskIdRnumArr.length < 1) {
+                new_r_num = r_num + 1
+                // console.log(r_num)
             } else {
-            console.log(`The user ${username} is not in the app_permit_create of the app ${applicationName} and hence cannot create tasks`)  
-            return res.send({
-                code: "CT01"
-            }) 
-        }
-        // console.log(permitted)
+                new_r_num = Math.max(...taskIdRnumArr) + 1 // Increment the Rnumber based on the largest exisitng one
+            }
 
-        // Get existing task_ids of tasks in the app
-        let tasksArr = await getAppTaskIds(applicationName) //  [ { task_id: 'NewTestApp_57' }, { task_id: 'NewTestApp_58' } ]
-        //let tasksArr = [ { task_id: 'KlookMVP_1' }, { task_id: 'KlookMVP_2' } ]
-        //console.log(tasksArr)
+            // Construct task_id: <app_acronym>_<app_rnumber>
+            let task_id = `${applicationName}_${new_r_num}`;
 
-        let taskIdRnumArr = []
-        tasksArr.forEach((task) => {
-            taskIdRnumArr.push(parseInt(task.task_id.split("_")[1]))
-        })
-        //console.log(taskIdRnumArr)
+            let today = new Date();
+            let dd = String(today.getDate()).padStart(2, '0');
+            let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            let yyyy = today.getFullYear();
 
-        // Get r_number of app with helper method
-        let r_num = await getRnumber(applicationName)
-        let new_r_num
-        if (taskIdRnumArr.length < 1) {
-            new_r_num = r_num + 1
-            // console.log(r_num)
-        } else {
-            new_r_num = Math.max(...taskIdRnumArr) + 1 // Increment the Rnumber based on the largest exisitng one
-        }
+            let hours = today.getHours()
+            let mins = today.getMinutes()
+            let seconds = today.getSeconds()
 
-        // Construct task_id: <app_acronym>_<app_rnumber>
-        let task_id = `${applicationName}_${new_r_num}`;
+            today = yyyy + '-' + mm + '-' + dd;
 
-        let today = new Date();
-        let dd = String(today.getDate()).padStart(2, '0');
-        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        let yyyy = today.getFullYear();
+            // Validation: Regex to validate user input
+            const task_nameRegexp = /^(?! )[A-Za-z0-9._\s]{2,20}(?<! )$/          // only alphanumeric, dots, underscores, spaces in between, no leading & trailing spaces, min 2 mx 20 chars
 
-        let hours = today.getHours()
-        let mins = today.getMinutes()
-        let seconds = today.getSeconds()
+            if (!taskName.match(task_nameRegexp)) {
+                return res.status(200).send({
+                    success: false,
+                    message: 'Please give a valid task name'
+                })
+            }
 
-        today = yyyy + '-' + mm + '-' + dd;
+            // Construct task notes string
+            let task_notes = `${username} has created the task: ${taskName} [${today} ${hours}:${mins}:${seconds}]`
 
-        // Validation: Regex to validate user input
-        const task_nameRegexp = /^(?! )[A-Za-z0-9._\s]{2,20}(?<! )$/          // only alphanumeric, dots, underscores, spaces in between, no leading & trailing spaces, min 2 mx 20 chars
+            let task_name = taskName
+            let task_description = taskDescription
+            let task_plan = "" // At task creation, there will be no plan
+            let task_app_acronym = applicationName
+            let task_state = "open"
+            let task_creator = username
+            let task_owner = username
+            let task_createdate = today
 
-        if (!taskName.match(task_nameRegexp)) {
-            return res.status(200).send({
-                success: false,
-                message: 'Please give a valid task name'
+            // Increase the current app_rnumber to prep for incrementing the app_rnumber in the db
+            let new_app_rnumber = r_num + 1
+
+            let new_task = {
+                task_id,
+                task_name,
+                task_description,
+                task_notes,
+                task_plan,
+                task_app_acronym,
+                task_state,
+                task_creator,
+                task_owner,
+                task_createdate
+            }
+            //console.log(new_task)
+            //res.send(new_task)
+
+            // Create new task
+            db.query(`insert into tasks (
+                task_id,
+                task_name,
+                task_description,
+                task_notes,
+                task_plan,
+                task_app_acronym,
+                task_state,
+                task_creator,
+                task_owner,
+                task_createdate
+            ) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [
+                task_id,
+                task_name,
+                task_description,
+                task_notes,
+                task_plan,
+                task_app_acronym,
+                task_state,
+                task_creator,
+                task_owner,
+                task_createdate
+            ], (err, results) => {
+                if (err) {
+                    res.send({
+                        code: "CT01"
+                    })
+                } else {
+                    res.send({
+                        code: "CT00",
+                        task_id: task_id,
+                    })
+
+                    // Increment application r_number
+                    db.query(`UPDATE applications 
+                    SET app_rnumber = ?
+                    WHERE app_acronym = ?`, [new_app_rnumber, task_app_acronym], (err, results) => {
+                        if (err) {
+                            console.log("Could not increase app_rnumber")
+                            console.log(err)
+                        } else {
+                            //console.log(results)
+                            console.log(`r_number of ${task_app_acronym} increased from ${r_num} to ${new_app_rnumber}`)
+                        }
+                    })
+
+                    console.log(new_task)
+                }
+            })
+        } catch (err) {
+            console.log('Catch all activated in createTaskAuthenticated')
+            console.log(err)
+            res.send({
+                code: "CT99"
             })
         }
-
-        // Construct task notes string
-        let task_notes = `${username} has created the task: ${taskName} [${today} ${hours}:${mins}:${seconds}]`
-
-        let task_name = taskName
-        let task_description = taskDescription
-        let task_plan = "" // At task creation, there will be no plan
-        let task_app_acronym = applicationName
-        let task_state = "open"
-        let task_creator = username
-        let task_owner = username
-        let task_createdate = today
-
-        // Increase the current app_rnumber to prep for incrementing the app_rnumber in the db
-        let new_app_rnumber = r_num + 1
-
-        let new_task = {
-            task_id,
-            task_name,
-            task_description,
-            task_notes,
-            task_plan,
-            task_app_acronym,
-            task_state,
-            task_creator,
-            task_owner,
-            task_createdate
-        }
-        //console.log(new_task)
-        //res.send(new_task)
-
-        // Create new task
-        db.query(`insert into tasks (
-            task_id,
-            task_name,
-            task_description,
-            task_notes,
-            task_plan,
-            task_app_acronym,
-            task_state,
-            task_creator,
-            task_owner,
-            task_createdate
-        ) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [
-            task_id,
-            task_name,
-            task_description,
-            task_notes,
-            task_plan,
-            task_app_acronym,
-            task_state,
-            task_creator,
-            task_owner,
-            task_createdate
-        ], (err, results) => {
-            if (err) {
-                res.send({
-                    code: "CT03"
-                })
-            } else {
-                res.send({
-                    code: "CT00",
-                    task_id: task_id,
-                })
-
-                // Increment application r_number
-                db.query(`UPDATE applications 
-                SET app_rnumber = ?
-                WHERE app_acronym = ?`, [new_app_rnumber, task_app_acronym], (err, results) => {
-                    if (err) {
-                        console.log("Could not increase app_rnumber")
-                        console.log(err)
-                    } else {
-                        //console.log(results)
-                        console.log(`r_number of ${task_app_acronym} increased from ${r_num} to ${new_app_rnumber}`)
-                    }
-                })
-
-                console.log(new_task)
-            }
-        })
-
     }
-})
+}
 
 // @desc    Get tasks by state
 // @route   /api//tasks/getTasksByState
@@ -284,7 +304,7 @@ const getTasksByState = catchAsyncErrors(async (req, res) => {
         db.query(`select * from tasks where task_app_acronym = ? and task_state = ?`, [applicationName, taskState], (err, results) => {
             if (err) {
                 res.send({
-                    code: "GT02"
+                    code: "GT01"
                 })
             } else {
                 res.send({
@@ -370,11 +390,11 @@ const promoteTask2Done = catchAsyncErrors(async (req, res) => {
         if (user_groups.includes(app_permit_doing)) {
             permitted = true
             console.log(`The user ${username} is in the app_permit_doing of the app ${app_acronym} and hence is able to promote tasks to Done`)
-            } else {
-            console.log(`The user ${username} is not in the app_permit_doing of the app ${app_acronym} and hence cannot promote tasks to Done`)  
+        } else {
+            console.log(`The user ${username} is not in the app_permit_doing of the app ${app_acronym} and hence cannot promote tasks to Done`)
             return res.send({
                 code: "PT01"
-            }) 
+            })
         }
 
         // Get existing task_notes of the task to append the new_note to the string of task_notes
@@ -436,7 +456,7 @@ const promoteTask2Done = catchAsyncErrors(async (req, res) => {
                 WHERE task_id = ?`, [new_state, username, updated_task_notes, taskID], (err, results) => {
                 if (err) {
                     res.send({
-                        code: "PT02"
+                        code: "PT01"
                     })
                 } else {
                     res.send({
