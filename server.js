45ffb4eb30fd0d384cede41eb25763c7cf6722eb
helen-tan <helentanmz0909@gmail.com>
2022-11-16 11:07:09 +0800

    Changed port to 8000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 
 // Route imports
 const taskRoutes = require('./routes/taskRoutes')
