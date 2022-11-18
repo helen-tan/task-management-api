8d847d38132b3c5b859a5dff69fe40015bf52199
helen-tan <helentanmz0909@gmail.com>
2022-11-18 10:56:38 +0800

    Handle error for malformed json body in request, routes that don't exist/incorrect parameters in url

--- server.js
+++ server.js
@@ -14,7 +14,32 @@ app.get('/', (req, res) => {
 
 // Middlewares
 app.use(express.json()) // allow to send raw json
-app.use(express.urlencoded({ extended: false })) // accept urlencoded form
+app.use(express.urlencoded({ extended: true })) // accept urlencoded form
+
+// Catch incorrect json format
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        console.error(err);
+        return res.status(400).send({ code: "AA88" }); 
+        // return res.status(400).send({ status: 404, message: err.message }); // Bad request
+    }
+    next();
+});
+
+// Catch all routes that don't exist (includes special chars. Except % for some reason..)
+app.all("*", (req, res, next) => {
+    res.send({ code: "AA99" })
+})
+
+// Catch incorrect passing of parameters in URL (catch % in url)
+app.use((err, req, res, next) => {
+    try {
+        decodeURIComponent(req.path)
+    } catch (err) {
+        return res.send({ code: "AA99" })
+    }
+    next()
+})
 
 app.use('/api/tasks', taskRoutes)
 
