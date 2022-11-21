4d415b95d336003b7eef8a9d6ba3dd2b37c55fcc
helen-tan <helentanmz0909@gmail.com>
2022-11-21 11:16:11 +0800

    Change get to post

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { createTask, getTasksByState, promoteTask2Done } = require('../controllers/taskController')
 
 router.post('/createTask', createTask)
-router.get('/getTasksByState', getTasksByState)
+router.post('/getTasksByState', getTasksByState)
 router.post('/promoteTask2Done', promoteTask2Done)
 
 module.exports = router
